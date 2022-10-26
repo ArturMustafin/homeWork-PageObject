40989374360a7ca9af38dcff3b3170d28ebcaf06
artur <artur5mustafin@gmail.com>
2022-10-27 01:51:59 +0300

    add capabilities

--- src/test/java/com/demoqa/tests/BaseTest.java
+++ src/test/java/com/demoqa/tests/BaseTest.java
@@ -19,14 +19,16 @@ public class BaseTest {
         Configuration.browserVersion = System.getProperty("browserVersion", "chrome");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
 
-        if (System.getProperty("remote") != null) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-            Configuration.browserCapabilities = capabilities;
-            Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        }
+//        if (System.getProperty("remote") != null) {
+//            DesiredCapabilities capabilities = new DesiredCapabilities();
+//            capabilities.setCapability("enableVNC", true);
+//            capabilities.setCapability("enableVideo", true);
+//
+//            Configuration.browserCapabilities = capabilities;
+//            Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+//        }
     }
 
     @AfterEach
